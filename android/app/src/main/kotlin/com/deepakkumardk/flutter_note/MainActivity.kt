b6eec5725c66995f478803961b230a67f97dcc43
Deepak Kumar <dpk950@gmail.com>
2019-04-25 10:05:33 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/deepakkumardk/flutter_note/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.deepakkumardk.flutter_note
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
